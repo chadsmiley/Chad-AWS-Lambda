b18811f4353e5cfda7622ec0b603671ffc2689bf
chadsmiley <chadsmiley@hotmail.com>
2017-12-12 14:22:50 -0600

    Update State.cs

--- Chad AWS Lambda/State.cs
+++ Chad AWS Lambda/State.cs
@@ -10,6 +10,7 @@ namespace Chad AWS Lambda
     public class State
     {
         /// <summary>
+        /// test
         /// Input value when starting the execution
         /// </summary>
         public string Name { get; set; }
